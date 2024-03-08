a637f2104385ed65f9ef3bd0d819a0d9bb613fe8
2ndZorua <485892@student.fontys.nl>
2024-03-08 11:32:06 +0100

    Added a large text widget

---
+++ lib/widgets/text/large.dart
@@ -0,0 +1,21 @@
+import "package:flutter/material.dart";
+
+class TextLarge extends StatelessWidget {
+  final String text;
+  final TextStyle? style;
+
+  const TextLarge({ required this.text, this.style, super.key });
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle? baseStyle = Theme.of(context).textTheme.bodyLarge;
+    return Text(
+      text,
+      style: baseStyle?.copyWith(
+        color: style?.color,
+        fontWeight: style?.fontWeight,
+        fontSize: style?.fontSize
+      )
+    );
+  }
+}
