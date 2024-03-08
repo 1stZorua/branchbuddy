d13e4877c53b41f9900316bcd93089a7c30edce0
2ndZorua <485892@student.fontys.nl>
2024-03-08 11:32:23 +0100

    Added a small text widget

---
+++ lib/widgets/text/small.dart
@@ -0,0 +1,21 @@
+import "package:flutter/material.dart";
+
+class TextSmall extends StatelessWidget {
+  final String text;
+  final TextStyle? style;
+
+  const TextSmall({ required this.text, this.style, super.key });
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle? baseStyle = Theme.of(context).textTheme.bodySmall;
+    return Text(
+      text,
+      style: baseStyle?.copyWith(
+        color: style?.color,
+        fontWeight: style?.fontWeight,
+        fontSize: style?.fontSize
+      )
+    );
+  }
+}
