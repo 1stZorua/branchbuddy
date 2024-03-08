6c2fe1a92bc05faaca0308822a4ae2aa83cd1c74
2ndZorua <485892@student.fontys.nl>
2024-03-08 11:30:54 +0100

    Added a input field widget

---
+++ lib/widgets/input/field.dart
@@ -0,0 +1,32 @@
+import "package:flutter/material.dart";
+
+class InputField extends StatelessWidget {
+  final String placeholder;
+
+  const InputField({ 
+    required this.placeholder,
+    super.key 
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Theme.of(context).colorScheme.tertiary,
+      ),
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: placeholder,
+          hintStyle: const TextStyle(
+            color: Colors.black45,
+          ),
+          border: InputBorder.none, 
+          contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+        ),
+        style: Theme.of(context).textTheme.bodySmall
+      ),
+    );
+  }
+}
