123b43020bbec372dbf05b4ac91d24475e898fa3
2ndZorua <485892@student.fontys.nl>
2024-03-08 11:30:29 +0100

    Added a barrel file for widgets

---
+++ lib/widgets/index.dart
@@ -0,0 +1,22 @@
+// Buttons
+export "./buttons/primary.dart";
+export "./buttons/text.dart";
+
+// Input
+export "./input/field.dart";
+
+// Modals
+export "./modal/bottom_sheet.dart";
+
+// Navigation
+export "./navigation/bottom_navigation_bar.dart";
+
+// Objects
+export "./objects/timeline/tile.dart";
+export "./objects/card.dart";
+export "./objects/line.dart";
+
+// Text
+export "./text/large.dart";
+export "./text/medium.dart";
+export "./text/small.dart";
