63a619000bb85fbc47388eb7c3dd18d9a1b3c9db
2ndZorua <485892@student.fontys.nl>
2024-03-08 11:29:08 +0100

    Added the splash screen

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,36 @@
+import "package:flutter/material.dart";
+import "package:go_router/go_router.dart";
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({ super.key });
+
+  @override
+  Widget build(BuildContext context){
+    Future.delayed(const Duration(seconds: 2))
+      .then((value) => GoRouter.of(context).goNamed("auth"));
+    return Stack(
+      children: [
+        Image(
+          image: const AssetImage("assets/images/splash_sample.png"),
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          fit: BoxFit.cover
+        ),
+        Scaffold(
+          backgroundColor: Theme.of(context).colorScheme.primary.withOpacity(0.5),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "branchbuddy",
+                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.white),
+                ),
+              ],
+            )
+          )
+        ),
+      ],
+    );
+  }
+}
