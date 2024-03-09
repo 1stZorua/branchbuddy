8e354331e3be884d16f034a9e6028ab19176441e
2ndZorua <485892@student.fontys.nl>
2024-03-09 14:12:38 +0100

    Added comments

--- lib/providers/navigation_provider.dart
+++ lib/providers/navigation_provider.dart
@@ -1,14 +1,22 @@
 import "package:flutter/material.dart";
 
+/*
+  Navigation provider to keep track of the current active route
+  that is part of the mobile navigation bar.
+*/
 class NavigationProvider extends ChangeNotifier {
+  // Index of the selected route
   int _selectedIndex = 1;
 
+  // Returns the current selected route index
   int get selectedIndex => _selectedIndex;
 
+  // Set the current selected route index
   void setSelectedIndex({
     required int index
   }) {
     _selectedIndex = index;
+    // Notify all the listeners of this change
     notifyListeners();
   }
 }
