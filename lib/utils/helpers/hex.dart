7a140b8130d45af5265cca198e6d2854c4476fb5
2ndZorua <485892@student.fontys.nl>
2024-03-09 14:13:39 +0100

    Added comments

--- lib/utils/helpers/hex.dart
+++ lib/utils/helpers/hex.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 
+// Converts given hex code to a Color widget
 Color hexToColor(String code) {
   return Color(int.parse(code.substring(1, 7), radix: 16) + 0xFF000000);
 }
