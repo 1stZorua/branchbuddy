ac871464a6c7c9c8ed477019fa04600eb52a6d88
2ndZorua <485892@student.fontys.nl>
2024-03-09 14:14:32 +0100

    Added comments

--- lib/widgets/text/medium.dart
+++ lib/widgets/text/medium.dart
@@ -1,7 +1,9 @@
 import "package:flutter/material.dart";
 
 class TextMedium extends StatelessWidget {
+  // Text
   final String text;
+  // Textstyle
   final TextStyle? style;
 
   const TextMedium({ required this.text, this.style, super.key });
